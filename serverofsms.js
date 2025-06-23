265f3110efc7a2d971a0afb1b0f483a83d4460fa
Abde lbassat Quaoubai <abquaoub@c4r9p1.1337.ma>
2025-06-23 17:49:09 +0100

    testinnnnnnnnn

--- serverofsms.js
+++ serverofsms.js
@@ -1,4 +1,1 @@
-pkg install wget unzip
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-arm.zip
-unzip ngrok-stable-linux-arm.zip
-chmod +x ngrok
+2yuvPNbpLMclFZvt4fM5iD872YZ_3nEiw65Tc69CSfQQgouxL
