28b789e6b97cef0e855202fbf8f236ffede251a0
Abde lbassat Quaoubai <abquaoub@c4r9p1.1337.ma>
2025-06-23 18:58:24 +0100

    testinnnnnnnnn

--- serverofsms.js
+++ serverofsms.js
@@ -1,1 +1,6 @@
-2yuvPNbpLMclFZvt4fM5iD872YZ_3nEiw65Tc69CSfQQgouxL
+pkg update && pkg upgrade
+pkg install wget unzip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip
+unzip ngrok-stable-linux-arm64.zip
+chmod +x ngrok
+./ngrok config add-authtoken 2yuvPNbpLMclFZvt4fM5iD872YZ_3nEiw65Tc69CSfQQgouxL
