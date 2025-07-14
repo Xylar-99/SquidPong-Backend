b0e4058f80132c40cd046606e5f5338c8b9c5bed
4bd3lb4554t <abdoqubai@gmail.com>
2025-07-14 14:20:02 +0100

    link account

--- backend/gateway/src/utils/utils.ts
+++ backend/gateway/src/utils/utils.ts
@@ -6,20 +6,12 @@ import { sendToService } from '../integration/api_calls';
 export async function   createAccount(data:any): Promise<any>
 {
     let password:string = data?.password ?? null;
-    let isExist = false;
-    {
-        const user = await prisma.user.findUnique({where : {email : data.email}})
-        if(!user)
-            return;
-        if(user.password)
-            password = user.password;
-        isExist = true;
-    }
+    const user = await prisma.user.findUnique({where : {email : data.email}})
 
     const dataUser = {email : data.email , password : password , username : data.username }
-    const Created = await prisma.user.upsert({ where: { email: data.email }, update: {password : dataUser.password}, create: dataUser });
+    const Created = await prisma.user.upsert({ where: { email: data.email }, update: password ? {password} : {} , create: dataUser });
 
-    if(!isExist)
+    if(!user)
     {
         const profile  = {...Created , ...data }
         await sendToService('http://user:4001/api/users/profile' , 'POST'  , null , profile)
