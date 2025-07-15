d696681e2e26b60075881da79944ca339161413c
4bd3lb4554t <abdoqubai@gmail.com>
2025-07-15 09:17:09 +0100

    link account

--- backend/gateway/src/controllers/proxy.controller.ts
+++ backend/gateway/src/controllers/proxy.controller.ts
@@ -5,6 +5,7 @@ import { sendToService } from '../integration/api_calls';
 
 async function proxyToUserService(req:FastifyRequest , res:FastifyReply)
 {
+    console.log(req.id);
     const data:any =  await sendToService(`http://user:4001${req.url}` , req.method , req.id , req.body)
     return res.send(data)
 }
@@ -28,7 +29,7 @@ async function proxyToGameService(req:FastifyRequest , res:FastifyReply)
     const method = req.method;
     const url = req.url;
 
-    const data:any =  await sendToService(`http://user:4001${url}` , method , req.body)
+    const data:any =  await sendToService(`http://game:4001${url}` , method , req.body)
     return res.send(data)
 }
 
