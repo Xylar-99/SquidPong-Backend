1fe056e3b61ca057604e9075221d17ca6ddfec9d
xylar-99 <xylar-99@Xylar-99.myguest.virtualbox.org>
2025-12-10 15:00:04 +0100

    fixed

--- backend/gateway/src/plugins/cors.ts
+++ backend/gateway/src/plugins/cors.ts
@@ -3,10 +3,15 @@ import cors from '@fastify/cors';
 
 export default async function corsPlugin(app: FastifyInstance) {
   await app.register(cors, {
-    origin: ['https://frontend-squid-ppong.vercel.app', 'http://localhost:3000'], // Specify exact origins
+    origin: (origin : any, cb : any) => {
+      // Allow all origins
+      cb(null, true);
+    },
     credentials: true, // Allow cookies and authorization headers
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['set-cookie']
+    exposedHeaders: ['set-cookie'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 }
